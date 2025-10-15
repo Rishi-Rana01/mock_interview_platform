cfc3e05c624e2c28033a652fba13cd1d44b45415
Rishi Rana <rishirana012354@gmail.com>
2025-10-15 23:30:53 +0530

    Home Page Initiate

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,21 @@
+import Link from 'next/link'
+import Image from 'next/image'
 import React, { ReactNode } from 'react'
 
-const RootLayout = ({children}:{children: ReactNode}) => {
+const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div>
+    <div className='root-layout'>
+
+      <nav>
+        <Link href="/" className='flex items-center gap-2' >
+          <Image src='/logo.svg' alt='Logo' width={38} height={32} />
+          <h2 className='text-primary-100'>PrepFast</h2>
+        </Link>
+      </nav>
+
       {children}
+      
+
     </div>
   )
 }
