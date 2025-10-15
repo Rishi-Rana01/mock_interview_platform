6a3ad1fb14a93d43d7d9494d50ab83d4dc10439f
Rishi Rana <rishirana012354@gmail.com>
2025-10-15 22:26:55 +0530

    Auth Pages

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,11 +1,9 @@
+import AuthForm from '@/components/AuthForm'
 import React from 'react'
 
 const page = () => {
-  return (
-    <div>
-      Page
-    </div>
-  )
+  return <AuthForm type="sign-in" />
+    
 }
 
 export default page
