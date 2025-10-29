81aa68d02218cb5f3ba19974c71204d02f0b01dd
Rishi Rana <rishirana012354@gmail.com>
2025-10-29 20:03:35 +0530

    feat: implement feedback generation and display in interview feedback page

---
+++ app/(root)/interview/[id]/feedback/page.tsx
@@ -0,0 +1,59 @@
+import Agent from '@/components/Agent';
+import { getCurrentUser } from '@/lib/actions/auth.action';
+import { getFeedbackByInterviewId, getInterviewsById } from '@/lib/actions/general.action';
+import { redirect } from 'next/navigation';
+import { getRandomInterviewCover } from "@/lib/utils";
+import Image from 'next/image';
+import React from 'react'
+import DisplayTechicons from '@/components/DisplayTechicons';
+
+const page = async ({ params }: RouteParams) => {
+  const { id } = await params;
+  const user = await getCurrentUser();
+
+  const interview = await getInterviewsById(id);
+  if (!interview) redirect('/');
+
+  const feedback = await getFeedbackByInterviewId({
+    interviewId: id,
+    userId: user?.id!,
+  })
+
+  console.log('Feedback:', feedback);
+
+  return (
+    <>
+      <div className="flex flex-row gap-4 justify-between">
+        <div className="flex flex-row gap-4 items-center max-sm:flex-col">
+          <div className="flex flex-row gap-4 items-center">
+            <Image
+              src={getRandomInterviewCover()}
+              alt="cover-image"
+              width={40}
+              height={40}
+              className="rounded-full object-cover size-[40px]"
+            />
+            <h3 className="capitalize">{interview.role} Interview</h3>
+          </div>
+
+          <DisplayTechicons techStack={interview.techstack} />
+        </div>
+
+        <p className="bg-dark-200 px-4 py-2 rounded-lg h-fit">
+          {interview.type}
+        </p>
+      </div>
+
+      <Agent
+        userName={user?.name!}
+        userId={user?.id}
+        interviewId={id}
+        type="interview"
+        questions={interview.questions}
+        feedbackId={feedback?.id}
+      />
+    </>
+  );
+}
+
+export default page
--- components/Agent.tsx
+++ components/Agent.tsx
@@ -7,6 +7,7 @@ import { useState, useEffect } from 'react';
 import React from 'react';
 import { vapi } from '@/lib/vapi.sdk';
 import { interviewer } from '@/constants';
+import { createFeedback } from '@/lib/actions/general.action';
 
 enum CallStatus {
     INACTIVE = 'INACTIVE',
@@ -63,10 +64,13 @@ const Agent = ({ userName, userId, type, interviewId, questions }: AgentProps) =
     const handleGenerateFeedback = async (messages: SavedMessage[]) => {
         console.log('Generated Feedback here.');
 
-        const { success, id } = {
-            success: true,
-            id: 'feedback-id'
-        }
+
+        const { success, feedbackId:id } = await createFeedback({
+            interviewId: interviewId!,
+            userId: userId!,
+            transcript: messages,
+        })
+
         if (success && id) {
             router.push(`/interview/${interviewId}/feedback`);
         } else {
